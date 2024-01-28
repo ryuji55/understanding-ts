f595f01a9cf9fc4ec353f440317464738fa36754
ryuji55 <rt.october.2@icloud.com>
2024-01-28 13:29:47 +0900

    Fix:Class

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,34 @@
-console.log("ここにコードを追加します");
+class Department {
+  private employees: string[] = [];
+
+  constructor(private readonly id: string, private readonly name: string) {}
+
+  describe(this: Department) {
+    console.log("Department: " + this.id, this.name);
+    console.log(`${this.id}: ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+}
+
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+}
+
+const it = new ITDepartment("it1", ["itMax"]);
+console.log("it", it);
+
+const accounting = new Department("1", "Accounting");
+accounting.describe();
+
+accounting.addEmployee("Max");
+
+console.log(accounting);
+
+// accounting.employees[2] = "Anna";
